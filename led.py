447c64cc96f9ea97328e1c8ebeb317367a4a6db1
GvidoJ <gvidojakobsons46@gmail.com>
2019-11-05 10:27:55 +0200

    Update led.py

--- led.py
+++ led.py
@@ -1,2 +1,21 @@
-man patik mainkrafts
-es esmu gvidcars
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarning(False)
+GPIO.setup(18,GPIO.OUT)
+
+counter = 0
+try:
+    while counter < 30:
+        print ('LED 1 on')
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(1)
+        print ('LED off')
+        GPIO.output(18,GPIO.LOW)
+        time.sleep(1)
+        print ('kaut kas jauns')
+        counter = counter + 1
+except KeyboardInterrupt:
+    print ('\n'), counter
+finally:
+    GPIO.cleanup()
