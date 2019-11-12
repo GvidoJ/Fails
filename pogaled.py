46de801fde76f73b46b1b1a049bf3153a4c00215
GvidoJ <gvidojakobsons46@gmail.com>
2019-11-12 10:27:28 +0200

    Create pogaled.py

---
+++ pogaled.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+GPIO.setup(21, GPIO.IN GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(16, GPIO.IN, GPIO.PUD_UP)
+
+try:
+    while True:
+        button_state1 = GPIO.input(21)
+        button_state2 = GPIO.input(20)
+        button_state3 = GPIO.input(16)
+    if button_state1 == GPIO.LOW:
+       button_state2 == GPIO.LOW:
+       button_state3 == GPIO.LOW:
+    print('LED 1 on')
+    GPIO.output(26,GPIO.HIGH)
+    GPIO.output(19,GPIO.HIGH)
+    GPIO.output(13,GPIO.HIGH)
+    time.sleep(1)
+    print ('Led off')
+except:
+    GPIO.cleanup()
